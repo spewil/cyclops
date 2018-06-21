f81d658f87937fa6b42f1f80d8a2c518b4274eca
spencer wilson <spencerryanwilson@gmail.com>
2018-06-21 10:22:48 +0100

    working simple webcam GUI

--- lib/GUI.py
+++ lib/GUI.py
@@ -1,47 +1,83 @@
 import sys
-from PyQt5.QtCore import QCoreApplication
-from PyQt5.QtGui import *
-from PyQt5.QtGui import QIcon
-from PyQt5.QtWidgets import QAction
-from PyQt5.QtWidgets import QApplication, QWidget, QMainWindow, QPushButton
 
+from PyQt5.QtCore import QDir, Qt, QSize, QThread, QObject, pyqtSignal, pyqtSlot
+from PyQt5.QtGui import QImage, QPixmap
+from PyQt5.QtWidgets import QWidget, QMainWindow, QApplication, QLabel, QPushButton
 
-class window(QMainWindow):
+import cv2
+import numpy as np
 
+# worker.py
+class Worker(QObject):
+    imageready = pyqtSignal(QImage)
+
+    def __init__(self, parent=None, **kwargs):
+        super().__init__(parent, **kwargs)
+
+    @pyqtSlot()
+    def displayImage(self): # A slot takes no params
+        cap = cv2.VideoCapture(0)
+        while True:
+            ret, frame = cap.read()
+            rgbImage = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+            convertToQtFormat = QImage(rgbImage.data, rgbImage.shape[1], rgbImage.shape[0], QImage.Format_RGB888)
+            p = convertToQtFormat.scaled(640, 480, Qt.KeepAspectRatio)
+            # emit the signal
+            self.imageready.emit(p)
+
+class GUI(QWidget):
     def __init__(self):
-        super(window, self).__init__()
-        self.setGeometry(50, 50, 500, 300)
-        self.setWindowTitle('pyqt5 Tut')
-        # self.setWindowIcon(QIcon('pic.png'))
+        super().__init__()
+        self.title = 'PyQt5 Video'
+        self.left = 100
+        self.top = 100
+        self.width = 640
+        self.height = 480
 
-        extractAction = QAction('&Get to the choppah', self)
-        extractAction.setShortcut('Ctrl+Q')
-        extractAction.setStatusTip('leave the app')
-        extractAction.triggered.connect(self.close_application)
+        self.label = QLabel(self)
 
-        self.statusBar()
+        # 1 - create Worker and Thread inside the Form
+        self.worker = Worker()  # no parent!
+        self.thread = QThread()  # no parent!
 
-        mainMenu = self.menuBar()
-        fileMenu = mainMenu.addMenu('&File')
-        fileMenu.addAction(extractAction)
+        # 2 - Connect Worker`s Signals to Form method slots to post data.
+        self.worker.imageready.connect(self.display_image)
 
-        self.home()
+        # 3 - Move the Worker object to the Thread object
+        self.worker.moveToThread(self.thread)
 
-    def home(self):
-        btn = QPushButton('quit', self)
-        btn.clicked.connect(self.close_application)
+        # # 4 - Connect Worker Signals to the Thread slots
+        self.thread.finished.connect(self.thread.quit)
 
-        btn.resize(btn.sizeHint())
-        btn.move(0, 100)
-        self.show()
+        # 5 - Connect Thread started signal to Worker operational slot method
+        self.thread.started.connect(self.worker.displayImage)
 
-    def close_application(self):
-        print('whooo so custom')
-        sys.exit()
+        # * - Thread finished signal will close the app if you want!
+        self.thread.finished.connect(app.exit)
 
-def run():
-    app = QApplication(sys.argv)
-    Gui = window()
-    sys.exit(app.exec_())
+        # 6 - Start the thread
+        self.thread.start()
 
-run()
+        # 7 - Start the form
+        self.initUI()
+
+    def initUI(self):
+        self.setWindowTitle(self.title)
+        self.setGeometry(self.left, self.top, self.width, self.height)
+        self.resize(700, 500)
+        # create a label
+        self.label.move(30, 10)
+        self.label.resize(640, 480)
+
+    def display_image(self, image):
+        pixmap_image = QPixmap(QPixmap.fromImage(image))
+        self.label.setPixmap(pixmap_image)
+        self.label.show()
+
+app = QApplication(sys.argv)
+window = GUI()
+window.show()
+sys.exit(app.exec_())
+
+# https://stackoverflow.com/questions/6783194/background-thread-with-qthread-in-pyqt
+# https://gist.github.com/jazzycamel/8abd37bf2d60cce6e01d
